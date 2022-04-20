20567cca14d43cbea11207a5551eb3abb764106a
cas1m1r <tyl3r5durd3n@gmail.com>
2022-04-20 18:34:37 -0400

    Update watcher.py
    adding option to serve squatter files

--- watcher.py
+++ watcher.py
@@ -1,5 +1,4 @@
 from colorama import init, Fore, Style
-from parser import levenshtein
 import multiprocessing
 import pandas as pd
 import dns.resolver
@@ -22,16 +21,41 @@ if not os.path.isfile(LOG):
 fB = Fore.LIGHTBLUE_EX
 fR = Fore.RED
 fW = Fore.WHITE
-fO = '\033[33m'
-fP = '\033[93m'
+fO = Fore.YELLOW
 fC = Fore.CYAN
 fG = Fore.GREEN
 fY = Fore.YELLOW
-BOLD = '\033[1m'
-OFF = '\033[0m'
+BOLD = Style.BRIGHT
+OFF = ''
 
 top1m = pd.read_csv('top-1m.csv')
 TOP_DOMAINS = list(top1m[:]['DOMAIN'])
+
+def levenshtein(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in range(size_x):
+        matrix [x, 0] = x
+    for y in range(size_y):
+        matrix [0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+    return matrix[size_x - 1, size_y - 1]
+
 
 def get_arecord_ip(host):
     ans = b''
@@ -50,7 +74,7 @@ def test_domain(dom_registered, dom_real, t):
         pass
     if 3 > score >= 0:
         name = dom_registered.replace('*.','')
-        log_msg = f'{t}{BOLD}{fP} {dom_registered} {fW}was registered {fW}[similar to {dom_real}? IP:{ip}]'
+        log_msg = f'{t} {dom_registered} was registered [similar to {dom_real}? IP:{ip}]'
         print(log_msg)
         open(LOG,'a').write(f"[{t}] {dom_registered} [similar to {dom_real}? IP:{ip})\n")
         # Maybe also show the Location data?
@@ -80,27 +104,29 @@ def print_callback(message, context):
        		
 			sus = False
 			threads = multiprocessing.Pool(10)
-			for real_domain in TOP_DOMAINS[0:1250]:
-				event = threads.apply_async(test_domain, (domain,f'{real_domain}', tsfmt))
-				if event.get(4):
-					sus = True
-					break
+			for real_domain in TOP_DOMAINS[0:7500]:
+				try:
+					event = threads.apply_async(test_domain, (domain,f'{real_domain}', tsfmt))
+					if event.get(3):
+						sus = True
+						break
+				except multiprocessing.TimeoutError:
+					pass
 			if not sus:
 				msg = f"[{tsfmt}] {domain} registered to {IP}\n"
-				if domain.split('.')[-1] in ['ru','cn','hk', 'kp']:
-					C = BOLD+fR
+				if domain.split('.')[-1] in ['ru','cn','hk', 'kp','ua']:
 					open(LOG,'a').write(msg)
-				elif domain.split('.')[-1] in ['party', 'download','trace','xin', 'stream']:
-					C = BOLD+fO
+				elif domain.split('.')[-1] in ['party', 'download','trace','xin', 'stream','cloud']:
 					open(LOG,'a').write(msg)
 				elif 'autodiscover' in domain.split('.'):
-					C = BOLD+fC
 					open(LOG,'a').write(msg)
-				else:
-					C = fG
-				print(f'{tsfmt}{C} {domain} {fW}was registered at {IP}{OFF}')
+				print(f'{tsfmt} {domain} was registered at {IP}')
 
 if __name__ == '__main__':
     logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
     certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
-    
+    if '--server' in sys.argv:
+        os.system('python3 -m http.server 9876')
+        # Use this to let other machines pull the squatter files on machine
+        # useful if running seperate nodes for filtering different types of
+        # domains or 'watcher.py' variants. 
