ff1c59050e9038df10c988f3f5c595600c01cefb
cas1m1r <tyl3r5durd3n@gmail.com>
2022-04-27 22:05:13 -0400

    moving things around

--- malcrawl.py
+++ malcrawl.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Firefox
 import datetime
 import requests
 import json
+import sys
 import os
 
 def error(msg):
@@ -40,42 +41,60 @@ class DataLoader:
 			PORT = int(config['port'])
 		else:
 			PORT = 8000
-		if 'remote' in config.keys():
-			date_label = datetime.datetime.now().strftime('%m%d%y')
-			LOG = os.path.join(f'squatters{date_label}.txt')
-			RMT = config['remote']
-			URL = f'http://{RMT}:{PORT}/{LOG}'
-			req = requests.get(URL)
-			if req.status_code == 200:
-				print(f'[+] Loaded DNS Records from {URL}')
-			else:
-				error(f'Unable to Get Data from {URL}')
-			log_data = req.text.split('\n')
-		elif 'local' in config.key():
-			print(f'[+] Loading DNS Records from locally saved data')
-			if not os.path.isfile(config['local']):
-				error(f'Unable to find Data at {config["local"]}')
-			log_data = open(config['local'],'r').read().split('\n')
-		return log_data
+		if 'files' in config.keys():
+				LOG = config['files']
+				# Multiple logs to parse
+				log_data = []
+		else:
+			LOG = f"squatters{datetime.datetime.now().strftime('%m%d%y')}.txt"
 
+		if len(LOG) > 1:
+			for logfile in LOG:
+				log_data.append(self.get_remote_log(config['remote'],PORT,logfile))
+		else:
+			# Pull a remote log
+			if 'remote' in config.keys():
+				log_data = self.get_remote_log(config['remote'], PORT, LOG)
+			elif 'local' in config.key():
+				log_data = self.get_local_log(config)
+			return log_data
+
+
+	def get_remote_log(self,rmt,port,log):
+		URL = f'http://{rmt}:{port}/{log}'
+		req = requests.get(URL)
+		if req.status_code == 200:
+			print(f'[+] Loaded DNS Records from {URL}')
+		else:
+			error(f'Unable to Get Data from {URL}')
+		return req.text.split('\n')
+
+	def get_local_log(self, config):
+		print(f'[+] Loading DNS Records from locally saved data')
+		if not os.path.isfile(config['local']):
+			error(f'Unable to find Data at {config["local"]}')
+		return open(config['local'],'r').read().split('\n')
 
 	def parse_log(self):
 		log = {}
-		for ln in self.raw_dns_data:
-			try:
-				fields = ln.split(' ')
-				date_day = fields[0].replace('[','')
-				date_time = fields[1].replace(']','')
-				dom_registered = fields[2]
-				registrant = fields[-1].replace('[','').replace(']','')
-				log[date_day+' '+date_time] = {'domain':dom_registered,
-											   'registrant': registrant}
-			except IndexError:
-				pass
-		print(f'[-] {len(log.keys())} Entries Parsed')
+		if type(self.raw_dns_data) != None:
+			for ln in self.raw_dns_data:
+				try:
+					fields = ln.split(' ')
+					date_day = fields[0].replace('[','')
+					date_time = fields[1].replace(']','')
+					dom_registered = fields[2]
+					registrant = fields[-1].replace('[','').replace(']','')
+					log[date_day+' '+date_time] = {'domain':dom_registered,
+												   'registrant': registrant}
+				except IndexError:
+					pass
+			print(f'[-] {len(log.keys())} Entries Parsed')
 		return log
 
 if __name__ == '__main__':
 	# Load Data from Remote Machines 
-	#interesting_site_data = DataLoader({'remote': 'CHANGE_ME'})
+	interesting_site_data = DataLoader({'remote': '192.168.1.155', 
+										'files': ['squatters042022.txt','squatters042122.txt','squatters042222.txt','squatters042322.txt']})
+
 	#squatter_data = DataLoader({'remote':'CHANGE_ME'})
